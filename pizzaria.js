ad6805ceaa681835210d680735a3d79ca29c31ed
André Luiz <133181905+1919anosandre@users.noreply.github.com>
2024-05-15 21:24:24 -0300

    Update pizzaria.js

--- pizzaria.js
+++ pizzaria.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const Caixa_Inicial = document.querySelector(".Caixa_Inicial");
     const Caixa_Cardapio = document.querySelector(".Caixa_Cardapio");
     const Caixa_Sobre = document.querySelector(".Caixa_Sobre");
+    const mobile_menu = document.querySelector(".mobile-menu");
+    const nav = document.querySelector(".nav-list");
 
     Inicio.addEventListener("click", () => {
         Caixa_Inicial.scrollIntoView({ behavior: "smooth" });
@@ -17,6 +19,14 @@ document.addEventListener("DOMContentLoaded", () => {
     Sobre.addEventListener("click", () => {
         Caixa_Sobre.scrollIntoView({ behavior: "smooth" });
     });
+
+    mobile_menu.addEventListener("click", () => {
+        Caixa_Inicial.style.zIndex = '0';
+        nav.style.zIndex = '2';
+
+    })
+
+
 });
 
 //evento meu pedido mostrar na tela ao clicar em pedir ja 
@@ -36,6 +46,51 @@ document.addEventListener('DOMContentLoaded', () => {
 let Dia = new Date();
 let exibir_dia = Dia.getDay();
 
-if(exibir_dia == 1){
-alert('Nao iremos atender hoje');
+if (exibir_dia == 1) {
+    alert('Nao iremos atender hoje');
 }
+
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+                    }s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
+
